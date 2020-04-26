0e779cc267795603af8809d75f5f26abd72fe9bf
Nick Fahrendorff <nick.fahrendorff@stud.htwk-leipzig.de>
2020-04-26 12:09:03 +0200

    useless method

--- src/main/java/de/test/git/controller/OtherController.java
+++ src/main/java/de/test/git/controller/OtherController.java
@@ -1,4 +1,5 @@
 package de.test.git.controller;
 
 public class OtherController {
+    private final String peter = "lustig";
 }
--- src/main/java/de/test/git/controller/SimpleController.java
+++ src/main/java/de/test/git/controller/SimpleController.java
@@ -8,12 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SimpleController {
 
-	@Value("${spring.application.name}")
+    @Value("${spring.application.name}")
     String appName;
- 
+
     @GetMapping("/")
     public String homePage(Model model) {
-        model.addAttribute("appName", appName);
-        return "home";
+        model.addAttribute("arsch", appName);
+        return "pimmel";
+    }
+
+    public int useless() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int d = 1;
+        int e = 1;
+        for (int i = 0; i < 90; i++) {
+            d = 0;
+        }
+
+        return a + b + c;
     }
 }
